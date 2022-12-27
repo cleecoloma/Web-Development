b32862c315a907f04bd4044f775b74a2eb84ea2f
Your Name <you@example.com>
2022-12-26 22:42:38 -0800

    change innerHTML text color

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -2,8 +2,10 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function () {
-        alert("I got clicked!");
-
+        this.style.color = "#FFFFFF";
         //What happens when button is clicked.
     });
 }
+
+// var audio = new Audio('sounds/tom-1.mp3')
+// audio.play();
