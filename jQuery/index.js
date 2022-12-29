e54aa6740e4c6cc620c0a4ca6a3c6df0c3e63180
Your Name <you@example.com>
2022-12-28 23:12:08 -0800

    initial commit for jQuery lesson

---
+++ jQuery/index.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//     $("h1").css("color", "red");
+// });
+
+$("h1").css("color", "green"); //as long as you put the javascript at the end of html
+
+$("h1").addClass("big-title margin-50"); //add class to change h1 based on css
+
+
