4b9633046af651462ea135ba5592dfdf2c449aa4
Your Name <you@example.com>
2023-01-22 22:36:14 -0800

    calculator example

---
+++ Calculator/calculator.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res) {
+    // res.sendFile("index.html");
+    // console.log(__dirname);
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req,res) {
+    // console.log(req.body);
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2)
+
+    var result = num1 + num2;
+    res.send("The calculation answer is " + result + ".");
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
