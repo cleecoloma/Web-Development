24accc771c429d9b1cc6278bb2298f6ff8a1ec41
Your Name <you@example.com>
2023-01-22 23:01:23 -0800

    added bmi calculator using node

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -13,10 +13,25 @@ app.get("/", function(req,res) {
 app.post("/", function(req,res) {
     // console.log(req.body);
     var num1 = Number(req.body.num1);
-    var num2 = Number(req.body.num2)
+    var num2 = Number(req.body.mum2)
 
     var result = num1 + num2;
     res.send("The calculation answer is " + result + ".");
+});
+
+
+app.get("/bmicalculator", function(req,res) {
+    // this is for BMI calculator
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req,res) {
+    // console.log(req.body);
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height)
+
+    var bmi = weight / (height * height);
+    res.send("Your BMI is " + bmi + ".");
 });
 
 app.listen(3000, function() {
