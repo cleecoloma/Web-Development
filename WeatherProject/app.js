3e9730c3e3e0b1748300e39f045746a365acd80c
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-01-30 21:39:27 -0800

    added city query capability to the weather app

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,29 +1,48 @@
 const express = require("express");
 const https = require("node:https");
+const bodyParser = require("body-parser")
 
 const app = express();
 
-app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Bremerton&appid=d7267d56b0088466dcf59931f3b03a61&units=metric"
-    https.get(url, function(response){
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function (req, res) {
+    console.log("Post request received!");
+    const query = req.body.cityName;
+    const apiKey = "d7267d56b0088466dcf59931f3b03a61";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    https.get(url, function (response) {
         console.log(response.statusCode);
-        response.on("data", function(data){
+        response.on("data", function (data) {
             const weatherData = JSON.parse(data); //changes data from hex to javascript object
             const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description
-            console.log(temp);
-            console.log(weatherDescription);
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celsius.</h1>");
+            res.write("<p>The weather is currently " + weatherDescription + ".</p>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
+            // console.log(temp);
+            // console.log(weatherDescription);
         })
 
     })
-    res.send("Server is up and running");
-})
+    // res.send("Server is up and running");
+
+});
 
 
 
 
-
-app.listen(3000, function() { 
+app.listen(3000, function () {
     console.log("Server is running at port 3000!")
 })
 
