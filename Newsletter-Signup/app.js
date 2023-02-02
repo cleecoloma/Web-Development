64ddb3f68bdf82b5b0074532a6354461116cbcb3
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-02-01 22:03:27 -0800

    added new project - signup newsletter

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,15 @@
+// jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html")
+});
+
+app.listen(3000, function(){
+    console.log("Server is running at port 3000!");
+})
