8d7516033501898e36e3a64cc44a1fa09c46a24b
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-02-06 21:05:52 -0800

    deployed code on Heroku and made minor code change

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -65,7 +65,7 @@ app.post("/failure", function(req, res){
 })
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running at port 3000!");
 });
 
