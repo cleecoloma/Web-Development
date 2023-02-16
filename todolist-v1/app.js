3552f0ed66e4ac763ec7af9ccdf348a3858cee0c
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-02-15 20:58:54 -0800

    added for loop to add new items and css

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,21 +3,39 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
+
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-    const listOfDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    day = listOfDays[currentDay];
-    // Another way to accomplish this task is to use if else statement
-    // Also we can use switch statement to do this
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItems: items
     })
 });
 
+app.post("/", function (req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+    // console.log(item);
+})
 
 
 app.listen(3000, function () {
