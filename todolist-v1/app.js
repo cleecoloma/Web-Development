6056e65986767fbc804eea98d4b9b468317698de
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-02-15 21:55:01 -0800

    added module functionality

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const getDate = require("./date");
+const date = require(__dirname + "/date.js")
+
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = ["Turn On Computer", "Open VSCode", "Start Coding"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = ["Turn On Computer", "Open VSCode", "Start Coding"];
 
 app.set("view engine", "ejs");
 
@@ -15,15 +18,7 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    const day = date.getDate();
 
     res.render("list", {
         listTitle: day,
@@ -32,7 +27,7 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
@@ -50,7 +45,7 @@ app.get("/work", function (req, res) {
 });
 
 app.post("/work", function (req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     workItems.push(item);
     res.redirect("/work");
 });
