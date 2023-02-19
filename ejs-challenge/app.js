a69889373579e4b1d1f75a787607b5a03ec2ceda
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-02-19 00:28:09 -0800

    added new posts to homepage

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -10,49 +10,62 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+const posts = [];
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.render("home", {
-    homeParagraphContent: homeStartingContent
+    homeParagraphContent: homeStartingContent,
+    posts: posts
   });
-})
+});
 
 app.get("/about", function (req, res) {
   res.render("about", {
     aboutParagraphContent: aboutContent
   });
-})
+});
 
 app.get("/contact", function (req, res) {
   res.render("contact", {
     contactParagraphContent: contactContent
   });
-})
+});
 
 app.get("/compose", function (req, res) {
   res.render("compose");
-})
+});
 
 app.post("/compose", function (req, res) {
-  const post = req.body.newPost;
-  console.log(post);
-})
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = req.params.postName;
+
+  posts.forEach(function (post) {
+      const storedTitle = post.title;
+
+      if (storedTitle === requestedTitle) {
+        console.log("Match found!");
+    }
+  });
+}); 
 
 
 
 
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
