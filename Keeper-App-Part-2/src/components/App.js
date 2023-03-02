a8000e3a401957bbb8bbc452b4954618d3319cb9
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-03-01 22:29:09 -0800

    finished keeper app part 2 practice

---
+++ Keeper-App-Part-2/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(newNote => (<Note 
+      key={newNote.key}
+      title={newNote.title}
+      content={newNote.content}
+      />))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
