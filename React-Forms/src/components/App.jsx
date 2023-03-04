f236096eff8a2e67d949aac911887850b13ac7a4
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-03-03 22:25:04 -0800

    added react forms exercise

---
+++ React-Forms/src/components/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange() {
+    console.log(event.target.value);
+    setName(event.target.value);
+    // console.log(event.target.placeholder);
+    // console.log(event.target.type);
+  }
+
+  function handleClicked(event) {
+    setHeading(name);
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClicked}>
+        <input
+          onChange={handleChange}
+          value={name}
+          type="text"
+          placeholder="What's your name?"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
