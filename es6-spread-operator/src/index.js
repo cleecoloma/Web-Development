10e5c675b84a122b5dd3a4130967e69eb6ec9913
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-03-05 15:31:28 -0800

    added es6 spread operator

---
+++ es6-spread-operator/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const citrus = ["Lime", "Lemon", "Orange"];
+// const fruits = ["Apple", ...citrus, "Banana", "Coconut"]
+// console.log(fruits)
