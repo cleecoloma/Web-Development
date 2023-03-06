a64d3783db89e91b3a0edf0880e0be233e84774a
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-03-05 17:54:40 -0800

    completed react component tree practice

--- react-component-tree-practice/src/components/InputArea.jsx
+++ react-component-tree-practice/src/components/InputArea.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-function InputArea() {
+function InputArea(props) {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
   return (
     <div className="form">
       <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={addItem}>
+      <button onClick={() => {
+        props.addingItem(inputText);
+        setInputText("");
+          } 
+        }>
         <span>Add</span>
       </button>
     </div>
