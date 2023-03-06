326a7cecd39504dac63579e682dcf5bcc0523c70
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-03-05 20:52:49 -0800

    added add note

--- Keeper-App-Part-3/src/components/CreateArea.jsx
+++ Keeper-App-Part-3/src/components/CreateArea.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [inputNote, setInputNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const {name, value} = event.target;
+
+    setInputNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function sendNote(event) {
+    props.onAdd(inputNote);
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={handleChange} name="title" placeholder="Title" value={inputNote.title} />
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={inputNote.content} />
+        <button onClick={sendNote}>Add</button>
       </form>
     </div>
   );
